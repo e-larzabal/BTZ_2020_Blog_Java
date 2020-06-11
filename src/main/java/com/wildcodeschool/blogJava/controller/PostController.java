c1128084f906cd572b6de71ee4e2da37ecf326ff
Jean-Loup BAREYRE <jlbareyre@gmail.com>
2020-06-11 12:08:00 +0200

    New controllers

--- src/main/java/com/wildcodeschool/blogJava/controller/PostController.java
+++
@@ -1,8 +0,0 @@
-package com.wildcodeschool.blogJava.controller;
-
-public class PostController {
-
-    
-    
-    
-}
--- src/main/java/com/wildcodeschool/blogJava/repository/PostRepositery.java
+++
@@ -1,5 +0,0 @@
-package com.wildcodeschool.blogJava.repository;
-
-public interface PostRepositery {
-    
-}
